e6ddc67bad70e720e565b3c44d40f08a6b42aa12
alexbanilean <alexandru.banilean@gmail.com>
2023-05-21 15:15:56 +0300

    Added DB config and create table queries

---
+++ src/repository/RepositoryHelper.java
@@ -0,0 +1,35 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RepositoryHelper {
+    private static RepositoryHelper instance;
+
+    private RepositoryHelper() {}
+
+    public static RepositoryHelper getRepositoryHelper() {
+        if (instance == null) {
+            instance = new RepositoryHelper();
+        }
+
+        return instance;
+    }
+
+    public void executeSql(Connection connection, String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(query);
+    }
+
+    public void executeUpdateSql(Connection connection, String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+    }
+
+    public ResultSet executeQuery(Connection connection, String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        return statement.executeQuery(query);
+    }
+}
